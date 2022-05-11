8e3675c8817db0ea8233dac06f7aaa9adac78786
Rehmet Yeshanew <ryfgmz87@gmail.com>
2022-05-11 21:27:27 +0300

    data scaling and exploration

--- clean_dataframe.py
+++ clean_dataframe.py
@@ -5,71 +5,37 @@ import matplotlib.pyplot as plt
 import matplotlib.pylab as plt
 import seaborn as sns
 
-all_data = pd.read_excel("teleco_excel_file.xlsx")
-all_data.head()
-# pd.set_option('display.max_columns', 50)
 
-# fd = pd.DataFrame(all_data)
-# fd.head()
-
-# df = fd.drop(['IMSI', 'IMEI','TCP DL Retrans. Vol (Bytes)', 'TCP UL Retrans. Vol (Bytes)', 'HTTP DL (Bytes)', 'HTTP UL (Bytes)','Nb of sec with 125000B < Vol DL', 'Nb of sec with 1250B < Vol UL < 6250B', 'Nb of sec with 31250B < Vol DL < 125000B', 'Nb of sec with 37500B < Vol UL', 'Nb of sec with 6250B < Vol DL < 31250B', 'Nb of sec with 6250B < Vol UL < 37500B'], axis=1)
-# df.iloc[:5 , 0:10]
-# df = df.fillna(method='ffill')
-
-
-# def plot_hist(df:pd.DataFrame, column:str, color:str)->None:
-#     # plt.figure(figsize=(15, 10))
-#     # fig, ax = plt.subplots(1, figsize=(12, 7))
-#     sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
-#     plt.title(f'Distribution of {column}', size=20, fontweight='bold')
-#     plt.show()
-    
-# def plot_count(df:pd.DataFrame, column:str) -> None:
-#     plt.figure(figsize=(12, 7))
-#     sns.countplot(data=df, x=column)
-#     plt.title(f'Distribution of {column}', size=20, fontweight='bold')
-#     plt.show()
-    
-# def plot_bar(df:pd.DataFrame, x_col:str, y_col:str, title:str, xlabel:str, ylabel:str)->None:
-#     plt.figure(figsize=(12, 7))
-#     sns.barplot(data = df, x=x_col, y=y_col)
-#     plt.title(title, size=20)
-#     plt.xticks(rotation=75, fontsize=14)
-#     plt.yticks( fontsize=14)
-#     plt.xlabel(xlabel, fontsize=16)
-#     plt.ylabel(ylabel, fontsize=16)
-#     plt.show()
-
-# def plot_heatmap(df:pd.DataFrame, title:str, cbar=False)->None:
-#     plt.figure(figsize=(12, 7))
-#     sns.heatmap(df, annot=True, cmap='viridis', vmin=0, vmax=1, fmt='.2f', linewidths=.7, cbar=cbar )
-#     plt.title(title, size=18, fontweight='bold')
-#     plt.show()
-
-# def plot_box(df:pd.DataFrame, x_col:str, title:str) -> None:
-#     plt.figure(figsize=(12, 7))
-#     sns.boxplot(data = df, x=x_col)
-#     plt.title(title, size=20)
-#     plt.xticks(rotation=75, fontsize=14)
-#     plt.show()
-
-# def plot_box_multi(df:pd.DataFrame, x_col:str, y_col:str, title:str) -> None:
-#     plt.figure(figsize=(12, 7))
-#     sns.boxplot(data = df, x=x_col, y=y_col)
-#     plt.title(title, size=20)
-#     plt.xticks(rotation=75, fontsize=14)
-#     plt.yticks( fontsize=14)
-#     plt.show()
-
-# def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
-#     plt.figure(figsize=(12, 7))
-#     sns.scatterplot(data = df, x=x_col, y=y_col, hue=hue, style=style)
-#     plt.title(title, size=20)
-#     plt.xticks(fontsize=14)
-#     plt.yticks( fontsize=14)
-#     plt.show()
+def removing_missing_values(df):
+    df = fd.drop(['IMSI', 'IMEI','TCP DL Retrans. Vol (Bytes)', 'TCP UL Retrans. Vol (Bytes)', 'HTTP DL (Bytes)', 'HTTP UL (Bytes)','Nb of sec with 125000B < Vol DL', 'Nb of sec with 1250B < Vol UL < 6250B', 'Nb of sec with 31250B < Vol DL < 125000B', 'Nb of sec with 37500B < Vol UL', 'Nb of sec with 6250B < Vol DL < 31250B', 'Nb of sec with 6250B < Vol UL < 37500B'], axis=1)
+    df["Bearer Id"] = df["Bearer Id"].fillna(df["Bearer Id"].mode()[0])
+    df["Start"] = df["Start"].fillna(method='ffill')
+    df["Start ms"] = df["Start ms"].fillna(method='ffill')
+    df["End"] = df["End"].fillna(method='ffill')
+    df["End ms"] = df["End ms"].fillna(method='ffill')
+    df["Dur. (ms)"] = df["Dur. (ms)"].fillna(method='ffill')
+    df["Total UL (Bytes)"] = df["Total UL (Bytes)"].fillna(df["Total UL (Bytes)"].mode()[0])
+    df["Total DL (Bytes)"] = df["Total UL (Bytes)"].fillna(df["Total DL (Bytes)"].mode()[0])
+    df["Nb of sec with Vol DL < 6250B"] = df["Nb of sec with Vol DL < 6250B"].fillna(df["Nb of sec with Vol DL < 6250B"].mode()[0]) 
+    df["Nb of sec with Vol UL < 1250B"] = df["Nb of sec with Vol UL < 1250B"].fillna(df["Nb of sec with Vol UL < 1250B"].mode()[0])
+    df["MSISDN/Number"] = df["MSISDN/Number"].fillna(df["MSISDN/Number"].mode()[0])
+    df["Avg RTT DL (ms)"] = df["Avg RTT DL (ms)"].fillna(df["Avg RTT DL (ms)"].mode()[0])
+    df["Avg RTT UL (ms)"] = df["Avg RTT UL (ms)"].fillna(df["Avg RTT UL (ms)"].mode()[0])
+    df["Avg Bearer TP DL (kbps)"] = df["Avg Bearer TP DL (kbps)"].fillna(df["Avg Bearer TP DL (kbps)"].mode()[0])
+    df["Avg Bearer TP UL (kbps)"] = df["Avg Bearer TP UL (kbps)"].fillna(df["Avg Bearer TP UL (kbps)"].mode()[0])
+    df["Activity Duration DL (ms)"] = df["Activity Duration DL (ms)"].fillna(df["Activity Duration DL (ms)"].mode()[0])
+    df["Activity Duration UL (ms)"] = df["Activity Duration UL (ms)"].fillna(df["Activity Duration UL (ms)"].mode()[0])
+    df["Dur. (ms).1"] = df["Dur. (ms).1"].fillna(df["Dur. (ms).1"].mode()[0])
+    df["Handset Manufacturer"] = df["Handset Manufacturer"].fillna(value='undefined')
+    df["Handset Type"] = df["Handset Type"].fillna(value='undefined')
+    df["Last Location Name"] = df["Last Location Name"].fillna(value='undefined')
+    df = df.fillna(value=0)
+    print(df)
 
 
-# #plot_hist(df, "Social Media DL (Bytes)", "blue")
 
-# plot_box(df, "Social Media DL (Bytes)", "Social Media DL (Bytes) Outliers")
+
+if __name__ == "__main__":
+    all_data = pd.read_excel("teleco_excel_file.xlsx")
+    fd = pd.DataFrame(all_data)
+    removing_missing_values(fd)
