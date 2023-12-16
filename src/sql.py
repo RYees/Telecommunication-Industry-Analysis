f1a8f2829762a6b3e5569febeb122913ff71440a
rehmet yeshanew <ryfgmz87@gmail.com>
2023-12-16 23:00:40 +0300

    sql exporting data

--- src/sql.py
+++ src/sql.py
@@ -1,34 +1,73 @@
 import pandas.io.sql as sqlio
 import psycopg2 as ps
 from sqlalchemy import text
-# import 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, Float, Table, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import pandas as pd
 
 def db_connection_psycopg():
-    # database connection using psycopg2
-    pgconn = ps.connect(dbname="slack",
-                    user= "postgres",
-                    password= "123",
-                    host="localhost",
-                    port="5432")
-    return pgconn
+    try:
+        # database connection using psycopg2
+        pgconn = ps.connect(dbname="telecom",
+                        user= "postgres",
+                        password= "123",
+                        host="localhost",
+                        port="5432")
+        return pgconn
+    except Exception as e:
+        return f'Error: {str(e)}'
+    
 
-def db_read_table_psycopg(pgconn):
-    sql = """ SELECT * FROM public.slack  """
-    df = sqlio.read_sql_query(sql, pgconn)
-    return df
+def db_read_table_psycopg(pgconn) -> None:
+    try:
+        sql = """ SELECT * FROM public.slack """
+        df = sqlio.read_sql_query(sql, pgconn)
+        return df
+    except Exception as e:
+        return f'Error: {str(e)}'
+    
 
 def db_connection_sqlalchemy():
-    engine = create_engine('postgresql+psycopg2://postgres:123@localhost/slack')
-    return engine
+    try:
+        engine = create_engine('postgresql+psycopg2://postgres:123@localhost/telecom')
+        return engine
+    except Exception as e:
+            return f'Error: {str(e)}'
+    
 
-def db_read_table_sqlalchemy(engine):
-    query = 'SELECT * FROM public.xdr_data'
-    crypto_data_df = pd.read_sql_query(query , engine)
-    return crypto_data_df
+def db_create_table(engine, tablename) -> None:
+    try:
+        meta = MetaData()
+        TableCreation = Table(
+            tablename, meta
+        )
+        meta.create_all(engine)
+    except Exception as e:
+        return f'Error: {str(e)}'
+    
 
-def db_filter_table_sqlalchemy(engine):
-    query = f"SELECT * FROM slack WHERE reaction_count > 4"
-    slack_data_df = pd.read_sql_query(query , engine)
-    return slack_data_df
+def db_table_insert_dataframe(df, tablename, engine):
+    try:
+        df.to_sql(tablename, engine, if_exists='replace', index=False)
+        return 'Successful'
+    except Exception as e:
+        return f'Error: {str(e)}'
+    
+
+def db_read_table_sqlalchemy(engine) -> None:
+    try:
+        query = 'SELECT * FROM public.xdr_data'
+        data_df = pd.read_sql_query(query , engine)
+        return data_df
+    except Exception as e:
+        return f'Error: {str(e)}'
+    
+
+def db_filter_table_sqlalchemy(engine) -> None:
+    try:
+        query = f"SELECT * FROM slack WHERE reaction_count > 4"
+        data_df = pd.read_sql_query(query , engine)
+        return data_df
+    except Exception as e:
+        return f'Error: {str(e)}'
